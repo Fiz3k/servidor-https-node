2f6b344bf18fd095e2558d20f210ccbfe3d605b6
Camila <camilagdowning1@gmail.com>
2026-04-21 14:02:24 -0400

    Ruta POST/LOGIN
    Se agrega ruta login con su respectivo usuario y password, ademas de la verificacion del estado del servidor

--- server.js
+++ server.js
@@ -11,11 +11,30 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Servidor HTTPS funcionando");
 });
+
 //ruta de prueba 
 app.get("/test", (req, res) => {
     res.json({ mensaje: "Ruta de prueba OK" });
 });
 
-// Exporta la app 
+// Ruta login 
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === "admin" && password === "1234") {
+        return res.json({ message: "Login exitoso" });
+    }
+
+    res.status(401).json({ message: "Credenciales incorrectas" });
+});
+
+// Estado del servidor 
+app.get("/status", (req, res) => {
+    res.json({
+        status: "OK",
+        secure: true
+    });
+});
+
 module.exports = app;
 
